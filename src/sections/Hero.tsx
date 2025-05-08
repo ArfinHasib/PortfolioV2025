118b1cf10268386e490fd17c888a50b7b1982d6d
ArfinHasib <arfinhasib21375@gmail.com>
2025-05-08 17:19:33 +0600

    Rings Effect

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,23 @@
 import memojiImage from '@/assets/images/memoji-computer.png';
 import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
+import grainImage from '@/assets/images/grain.jpg';
 
 export const HeroSection = () => {
 	return (
-		<div className="py-32 ">
+		<div className="py-32 md:py-48 lg:py-60 relative z-0">
+			<div
+				className="absolute inset-0 -z-30 opacity-5"
+				style={{
+					backgroundImage: `url(${grainImage.src})`,
+				}}
+			></div>
+			{/* Rings */}
+			<div className="size-[620px] hero-ring"></div>
+			<div className="size-[820px] hero-ring"></div>
+			<div className="size-[1020px] hero-ring"></div>
+			<div className="size-[1220px] hero-ring"></div>
+			{/* Rings */}
 			<div className="container">
 				<div className="flex flex-col items-center">
 					<Image src={memojiImage} className="size-[100px]" alt="Person peeking from behind laptop" />
@@ -13,21 +26,23 @@ export const HeroSection = () => {
 						<div className="text-sm font-medium">Available for new projects</div>
 					</div>
 				</div>
-				<h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
-					Building Exceptional User Experiences
-				</h1>
-				<p className="mt-4 text-center text-white/60">
-					I specialize in transforming designs into functional, high performing web applications. Let's
-					discuss your next project.
-				</p>
-				<div className="flex flex-col items-center">
+				<div className="max-w-lg mx-auto">
+					<h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+						Building Exceptional User Experiences
+					</h1>
+					<p className="mt-4 text-center text-white/60 md:text-lg">
+						I specialize in transforming designs into functional, high performing web applications. Let's
+						discuss your next project.
+					</p>
+				</div>
+				<div className="flex flex-col md:flex-row items-center mt-8 gap-4 justify-center">
 					<button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
 						<span className="font-semibold">Explore My Work</span>
 						<ArrowDown className="size-4" />
 					</button>
-					<button>
+					<button className="inline-flex gap-2 items-center border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
 						<span>👋</span>
-						<span>Let's Connect</span>
+						<span className="font-semibold">Let's Connect</span>
 					</button>
 				</div>
 			</div>
