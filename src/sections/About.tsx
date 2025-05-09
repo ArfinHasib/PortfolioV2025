1409913c23ab7d4f0ab33031813002e1233f0e84
ArfinHasib <arfinhasib21375@gmail.com>
2025-05-09 23:12:23 +0600

    About Section Complete

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,74 +1,152 @@
-import { SectionHeader } from '@/components/SectionHeader';
-import { Card } from '@/components/Card';
-import StarIcon from '@/assets/icons/star.svg';
-import bookImage from '@/assets/images/book-cover.png';
-import Image from 'next/image';
-import JavaScriptIcon from '@/assets/icons/square-js.svg';
-import HTMLIcon from '@/assets/icons/html5.svg';
-import CSSIcon from '@/assets/icons/css3.svg';
-import ReactIcon from '@/assets/icons/react.svg';
 import ChromeIcon from '@/assets/icons/chrome.svg';
+import CSSIcon from '@/assets/icons/css3.svg';
 import GithubIcon from '@/assets/icons/github.svg';
+import HTMLIcon from '@/assets/icons/html5.svg';
+import ReactIcon from '@/assets/icons/react.svg';
+import JavaScriptIcon from '@/assets/icons/square-js.svg';
+import bookImage from '@/assets/images/book-cover.png';
+import mapImage from '@/assets/images/map.png';
+import smileEmoji from '@/assets/images/memoji-smile.png';
+import { Card } from '@/components/Card';
+import { CardHeader } from '@/components/CardHeader';
+import { SectionHeader } from '@/components/SectionHeader';
+import { ToolboxItems } from '@/components/ToolboxItems';
+import Image from 'next/image';
 
 const toolboxItems = [
 	{
 		title: 'JavaScript',
-		icon: <JavaScriptIcon />,
+		iconType: JavaScriptIcon,
 	},
 	{
 		title: 'HTML5',
-		icon: <HTMLIcon />,
+		iconType: HTMLIcon,
 	},
 	{
 		title: 'CSS3',
-		icon: <CSSIcon />,
+		iconType: CSSIcon,
 	},
 	{
 		title: 'React',
-		icon: <ReactIcon />,
+		iconType: ReactIcon,
 	},
 	{
 		title: 'NextJS',
-		icon: <ChromeIcon />,
+		iconType: ChromeIcon,
 	},
 	{
 		title: 'GitHub',
-		icon: <GithubIcon />,
+		iconType: GithubIcon,
+	},
+];
+
+const hobbies = [
+	{
+		title: 'Painting',
+		emoji: '🎨',
+		left: '5%',
+		top: '5%',
+	},
+	{
+		title: 'Photography',
+		emoji: '📷',
+		left: '50%',
+		top: '5%',
+	},
+	{
+		title: 'Hiking',
+		emoji: '🥾',
+		left: '35%',
+		top: '45%',
+	},
+	{
+		title: 'Gaming',
+		emoji: '🎮',
+		left: '10%',
+		top: '35%',
+	},
+	{
+		title: 'Music',
+		emoji: '🎵',
+		left: '70%',
+		top: '40%',
+	},
+	{
+		title: 'Fitness',
+		emoji: '💪',
+		left: '5%',
+		top: '65%',
+	},
+	{
+		title: 'Reading',
+		emoji: '📔',
+		left: '45%',
+		top: '70%',
 	},
 ];
 
 export const AboutSection = () => {
 	return (
-		<div className="container pb-96">
-			<SectionHeader
-				eyebrow="About Me"
-				title="A Glimpse Into My World"
-				description="Learn more about who I am, what I do and what inspires me."
-			/>
-			<div>
-				<Card>
-					<div>
-						<StarIcon />
-						<h3>My Reads</h3>
-						<p>Explore the books shaping my perspectives.</p>
-					</div>
-					<Image src={bookImage} alt="Book Cover" />
-				</Card>
-				<Card>
-					<div>
-						<StarIcon />
-						<h3>My Toolbox</h3>
-						<p>Explore the technologies and tools I use to craft exceptional digital experiences.</p>
-					</div>
-					<div>
-						{toolboxItems.map((item) => (
-							<div key={item.title}>
-								<span>{item.icon}</span>
-								<span>{item.title}</span>
+		<div className="py-20 lg:py-28">
+			<div className="container">
+				<SectionHeader
+					eyebrow="About Me"
+					title="A Glimpse Into My World"
+					description="Learn more about who I am, what I do and what inspires me."
+				/>
+				<div className="mt-20 flex flex-col gap-6">
+					<div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+						<Card className="h-[320px] md:col-span-2 lg:col-span-1">
+							<CardHeader title="My Reads" description="Explore the books shaping my perspectives." />
+							<div className="w-40 mx-auto mt-2 md:mt-0">
+								<Image src={bookImage} alt="Book Cover" />
 							</div>
-						))}
+						</Card>
+						<Card className="h-[320px] md:col-span-3 lg:col-span-2">
+							<CardHeader
+								className=""
+								title="My Toolbox"
+								description="Explore the technologies and tools I use to craft exceptional digital experiences."
+							/>
+							<ToolboxItems items={toolboxItems} className="mt-6" />
+							<ToolboxItems
+								items={toolboxItems}
+								className="mt-6"
+								itemsWrapperClassName="-translate-x-1/2"
+							/>
+						</Card>
 					</div>
-				</Card>
+					<div className="grid gap-8 grid-cols-1 md:grid-cols-5 lg:grid-cols-3">
+						<Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+							<CardHeader
+								title="Beyond the Code"
+								description="Explore my interests and hobbies beyond the digital realm."
+								className="px-6 pt-6 py-6"
+							/>
+							<div className="relative flex-1">
+								{hobbies.map((hoby) => (
+									<div
+										key={hoby.title}
+										className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+										style={{
+											left: hoby.left,
+											top: hoby.top,
+										}}
+									>
+										<span className="font-medium text-gray-950">{hoby.title}</span>
+										<span className="font-semibold">{hoby.emoji}</span>
+									</div>
+								))}
+							</div>
+						</Card>
+						<Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+							<Image src={mapImage} alt="map" className="h-full w-full object-cover object-left-top" />
+							<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-white/70">
+								<Image src={smileEmoji} alt="smiling emoji" className="size-20" />
+							</div>
+						</Card>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
