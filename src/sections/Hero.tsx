6ff696c9024ec5c23e89fad6bc89f334cee52a0d
ArfinHasib <arfinhasib21375@gmail.com>
2025-05-12 21:34:25 +0600

    Small Changes and updates

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -74,7 +74,7 @@ export const HeroSection = () => {
 				</div>
 				<div className="max-w-lg mx-auto">
 					<h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-						Building Exceptional User Experiences
+						Building Exceptional User Experiences and User Interfaces
 					</h1>
 					<p className="mt-4 text-center text-white/60 md:text-lg">
 						I specialize in transforming designs into functional, high performing web applications. Let's
